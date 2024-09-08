59a194ac9db360285333dcd33948c4565081ec06
MohsinAJ2420 <mohsinl227@gmail.com>
2024-09-09 00:05:58 +0200

    Update useGameFlow to add finishGame functionality

--- scoreboardlibrary/src/Hooks/useGameFlow.js
+++ scoreboardlibrary/src/Hooks/useGameFlow.js
@@ -12,9 +12,16 @@ const useGameFlow = (gameId) => {
     dispatch({ type: "START_GAME", gameId });
   }, [gameId, dispatch]);
 
- 
+  const finishGame = useCallback(() => {
+    if (status === "Playing") {
+      setStatus("Finished");
+      dispatch({ type: "FINISH_GAME", gameId });
+    }
+  }, [status, gameId, dispatch]);
   useRandomTimeout(startGame, 2000, 5000); 
- 
+  useRandomTimeout(finishGame, 10000, 15000);
+  console.log(status)
+
   return { status };
 };
 
