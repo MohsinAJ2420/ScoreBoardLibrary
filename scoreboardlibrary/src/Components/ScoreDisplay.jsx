826659ae4e7efdca5720d730e3c95943ff1379c2
MohsinAJ2420 <mohsinl227@gmail.com>
2024-09-08 19:23:28 +0200

    add scoredisplay

---
+++ scoreboardlibrary/src/Components/ScoreDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ScoreDisplay = ({ homeScore, awayScore, homeTeam, awayTeam }) => {
+  return (
+    <div>
+      <h3>{homeTeam} vs {awayTeam}</h3>
+      <p>Score: {homeScore} - {awayScore}</p>
+    </div>
+  );
+};
+
+export default ScoreDisplay;
